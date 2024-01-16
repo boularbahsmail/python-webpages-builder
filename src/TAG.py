c7503fc5f99a10d232eac00aff9c6207123e7c69
Ismailium <qiranorane55@gmail.com>
2024-01-16 17:31:27 +0100

    Built Tag Generator

---
+++ src/TAG.py
@@ -0,0 +1,26 @@
+def generate():
+    tagType = input("Tag type (Inline, Bloc) : ")
+        
+    tagName = input("Tag (name) : ")
+    tagID = input("Tag (id) : ")
+    tagClass = input("Tag (class) : ")
+    attributes = ''
+
+    if (tagType == "Inline" or tagType == "inline" or tagType == "INLINE"):
+        attributesLenght = input("Number of attributes : ")
+
+        for i in range(int(attributesLenght)):
+            attributeName = input(f"Name of attribute number {i+1} : ")
+            attributeValue = input(f"Value of attribute number {i+1} : ")
+
+            attributeFormat = f'{attributeName}="{attributeValue}"'
+            attributes += attributeFormat + " "
+
+        Tag = f'<{tagName} {attributes}id="{tagID}" class="{tagClass}" />'
+        print(Tag)
+        
+    elif (tagType == "Bloc" or tagType == "bloc" or tagType == "BLOC"):
+        tagContent = input("Tag (content) : ")
+
+        Tag = f'<{tagName} id="{tagID}" class="{tagClass}">{tagContent}</{tagName}>'
+        print(Tag)
